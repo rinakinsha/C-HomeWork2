c1a442cd71ff26d9bbc6802941d9004e052763d1
iryna <rinakinsha@yndex.by>
2022-06-26 17:42:22 +0300

    Solution of task No. 13

---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿void ThirdDigit()
+{
+    Console.Write("Введите число: ");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if(number<0)
+    {
+        number = number*-1;
+    }
+    if (number>=100)
+    {
+        while (number>1000)
+        {
+            number = number/10;
+        }
+
+        int third = number%10;
+        Console.WriteLine("Третьей цифра Вашего числа: " + third);
+        
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+ThirdDigit();

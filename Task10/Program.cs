88392790087295a4e381dff678db88f8cb109f1b
iryna <rinakinsha@yndex.by>
2022-06-26 16:47:11 +0300

    Solution of task No. 10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿int SecondDigit()
+{
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("Сгенерированное число: " + number);
+
+    int twodigit = number / 10;
+    int second = twodigit % 10;
+
+    return second;
+}
+
+int result = SecondDigit();
+Console.WriteLine("Вторая цифра сгенерированного числа: " + result);
